7686c9bd07d8bfb40a0e06258d14c380d70cb729
Egor <egor.sh.2002@yandex.ru>
2022-04-21 12:27:08 +0300

    Добавил миникамеру на игрока

---
+++ Assets/Window_CharacterPortrait.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Window_CharacterPortrait : MonoBehaviour {
+
+    private Transform cameraTransform;
+    private Transform followTransform;
+
+    private void Awake() {
+        cameraTransform = transform.Find("camera");
+
+        transform.Find("closeBtn").GetComponent<Button_UI>().ClickFunc = Hide;
+
+        Hide();
+    }
+
+    private void Update() {
+        cameraTransform.position = new Vector3(followTransform.position.x, followTransform.position.y, Camera.main.transform.position.z);
+    }
+
+    public void Show(Transform followTransform) {
+        gameObject.SetActive(true);
+        this.followTransform = followTransform;
+    }
+
+    public void Hide() {
+        gameObject.SetActive(false);
+    }
+}
